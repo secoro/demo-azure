8cc8eb2eb54ef178657eb959830b840941bc3fbe
seanderoo <sean.roo@koop.overheid.nl>
2025-02-01 09:42:13 +0100

    added logging

--- src/main/java/com/example/demoazure/controller/GreetingController.java
+++ src/main/java/com/example/demoazure/controller/GreetingController.java
@@ -1,5 +1,6 @@
 package com.example.demoazure.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -7,12 +8,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Optional;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/greeting")
 public class GreetingController {
 
     @GetMapping
     public String greeting(@RequestParam Optional<String> name) {
+        log.info("Greeting requested '{}'", name);
         return String.format("Hello %s!", name.orElse("World"));
     }
 }
